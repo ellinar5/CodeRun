4f6158f1a1ff8fc40d1f6629514798cc9a32348a
ellina <157985958+ellinar5@users.noreply.github.com>
2026-03-01 13:56:55 +0700

    Create main.py

---
+++ Быстрый старт/2_Треугольник/main.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def main():
+    """
+    Проверка значений происходит так:
+    1) число_1 + число_2 > число_3
+    2) число_1 + число_3 > число_2
+    3) число_2 + число_3 > число_1
+    """
+
+    number_1 = int(input())
+    number_2 = int(input())
+    number_3 = int(input())
+
+    if (number_1 + number_2) > number_3 and (number_1 + number_3) > number_2 and (number_2 +number_3) > number_1:
+        print("YES")
+    
+
+    else:
+        print("NO")
+
+
+if __name__ == '__main__':
+    main()
